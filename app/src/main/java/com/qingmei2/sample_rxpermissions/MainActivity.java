23af882b35cb3143e8d7c60ffa332279e29ccd48
qing.mei <qing.mei@fenrir-inc.com.cn>
2017-06-19 19:40:38 +0800

    code commit

--- app/src/main/java/com/qingmei2/sample_rxpermissions/MainActivity.java
+++ app/src/main/java/com/qingmei2/sample_rxpermissions/MainActivity.java
@@ -1,13 +1,35 @@
 package com.qingmei2.sample_rxpermissions;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.qingmei2.sample_rxpermissions.rx01_permissions.RxPermissionsActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_permissions)
+    Button btnPermissions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_permissions)
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_permissions:
+                startActivity(new Intent(this, RxPermissionsActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/qingmei2/sample_rxpermissions/rx01_permissions/RxPermissionsActivity.java
@@ -0,0 +1,86 @@
+package com.qingmei2.sample_rxpermissions.rx01_permissions;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.annimon.stream.Optional;
+import com.jakewharton.rxbinding2.view.RxView;
+import com.qingmei2.sample_rxpermissions.R;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class RxPermissionsActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_01)
+    Button btn01;
+    @BindView(R.id.btn_02)
+    Button btn02;
+    @BindView(R.id.btn_03)
+    Button btn03;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx_permissions);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_01, R.id.btn_02, R.id.btn_03})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_01:
+                requestPermission();
+                break;
+            case R.id.btn_02:
+                requestPermission2();
+                break;
+            case R.id.btn_03:
+                requestSomePermissions();
+                break;
+        }
+    }
+
+    /**
+     * 直接请求权限
+     */
+    public void requestPermission() {
+        new RxPermissions(this)
+                .request(Manifest.permission.CAMERA)
+                .subscribe(this::dealGranted);
+    }
+
+    /**
+     * 事件触发请求权限
+     */
+    private void requestPermission2() {
+        RxView.clicks(btn02)
+                .compose(new RxPermissions(this).ensure(Manifest.permission.CAMERA))
+                .subscribe(this::dealGranted);
+    }
+
+    /**
+     * 一次请求多个权限并处理
+     */
+    private void requestSomePermissions() {
+        new RxPermissions(this)
+                .request(Manifest.permission.CAMERA,
+                        Manifest.permission.READ_PHONE_STATE)
+                .subscribe(this::dealGranted);
+    }
+
+    private void dealGranted(boolean value){
+        Optional.ofNullable(value)
+                .filter(value1 -> value1)
+                .ifPresentOrElse(aBoolean -> Toast.makeText(this, "获取权限成功", Toast.LENGTH_SHORT).show(),
+                        () -> Toast.makeText(this, "获取权限失败", Toast.LENGTH_SHORT).show());
+    }
+
+
+}
