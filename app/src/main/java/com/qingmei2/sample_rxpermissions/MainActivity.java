e8d522c096202a75993723cfa41327aa06cce3cc
qing.mei <qing.mei@fenrir-inc.com.cn>
2017-07-10 14:46:00 +0800

    code commit

--- app/src/main/java/com/qingmei2/sample_rxpermissions/MainActivity.java
+++ app/src/main/java/com/qingmei2/sample_rxpermissions/MainActivity.java
@@ -3,43 +3,35 @@ package com.qingmei2.sample_rxpermissions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 
+import com.jakewharton.rxbinding2.view.RxView;
 import com.qingmei2.sample_rxpermissions.rx01_permissions.RxPermissionsActivity;
 import com.qingmei2.sample_rxpermissions.rx02_cache.CacheActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.btn_permissions)
     Button btnPermissions;
+    @BindView(R.id.btn_cmd)
+    Button btnCmd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initView();
     }
 
-    @OnClick({R.id.btn_permissions,R.id.btn_cmd})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.btn_permissions:
-                startActivity(new Intent(this, RxPermissionsActivity.class));
-                break;
-            case R.id.btn_cmd:
-                startActivity(new Intent(this, CacheActivity.class));
-                break;
-        }
+    private void initView() {
+        RxView.clicks(btnPermissions)
+                .subscribe(v -> startActivity(new Intent(this, RxPermissionsActivity.class)));
+        RxView.clicks(btnCmd)
+                .subscribe(v -> startActivity(new Intent(this, CacheActivity.class)));
     }
-
-    public Button getMyView(){
-        return btnPermissions;
-    }
-
 
 }
